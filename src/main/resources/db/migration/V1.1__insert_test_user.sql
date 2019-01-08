ad1306e82bdd7d50f7dd6a0a43b594385d38cc69
wsj <scglwsj@163.com>
2019-01-09 01:53:12 +0800

    添加测试数据

---
+++ src/main/resources/db/migration/V1.1__insert_test_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `snap_up`.`user_info` (`id`, `name`, `gender`, `age`, `telephone`, `register_mode`, `third_party_id`) VALUES ('1', 'test user', '1', '21', '13579246810', 'phone', '');
+INSERT INTO `snap_up`.`user_password` (`id`, `user_id`, `encrypted_password`) VALUES ('1', '1', 'qweasdzxc');
