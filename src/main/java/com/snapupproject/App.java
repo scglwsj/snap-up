1803e2cda2c6d4fb722a3863ef197f4f6beff3cb
wsj <scglwsj@163.com>
2019-01-09 01:06:58 +0800

    删除入口处多余代码

--- src/main/java/com/snapupproject/App.java
+++ src/main/java/com/snapupproject/App.java
@@ -1,31 +1,12 @@
 package com.snapupproject;
 
-import com.snapupproject.dao.UserDOMapper;
-import com.snapupproject.dataobject.UserDO;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = {"com.snapupproject"})
-@RestController
 @MapperScan("com.snapupproject.dao")
 public class App {
-    @Autowired
-    private UserDOMapper userDOMapper;
-
-    @RequestMapping("/")
-    public String home() {
-        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
-        if (userDO == null) {
-            return "用户对象不存在！";
-        }
-
-        return "Hello " + userDO.getName() + "!";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
     }
